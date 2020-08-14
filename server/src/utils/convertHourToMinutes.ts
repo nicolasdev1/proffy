623c86dd33ea8ca7d07b62e88ad158fbe245f246
nicolasdev1 <nicolaspessoal@icloud.com>
2020-08-14 11:13:58 -0300

    Back-end da aplicação

---
+++ server/src/utils/convertHourToMinutes.ts
@@ -0,0 +1,9 @@
+function convertHourToMinutes(time: string) {
+  const [hours, minutes] = time.split(':').map(Number);
+
+  const timeInMinutes = (hours * 60) + minutes;
+
+  return timeInMinutes;
+}
+
+export default convertHourToMinutes;
